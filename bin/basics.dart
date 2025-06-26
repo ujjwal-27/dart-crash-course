ff6b5cd0614dd45424df9603537e82916397afcb
Ujjwal <ujjwalshresth.96@gmail.com>
2025-06-26 11:09:29 +0545

    dart basics

---
+++ bin/basics.dart
@@ -0,0 +1,20 @@
+void main() {
+  var count = 4;
+
+  for (var i = 0; i < count; i++) {
+    print("hello $i");
+  }
+
+  final age = 28;
+  print(age);
+
+  const isOpen = true;
+  // isOpen = false;
+
+  var division = age / 7;
+  print(division.toInt());
+
+  var name = "Ujjwal";
+
+  print("My name is $name");
+}
