bde16e0ab9d30fa8514b2f98975dc4fc862b3b0a
Ujjwal <ujjwalshresth.96@gmail.com>
2025-06-26 20:40:24 +0545

    type annotations

---
+++ bin/typeAnnotations.dart
@@ -0,0 +1,16 @@
+void main() {
+  String name = "Ujjwal";
+  print(name);
+
+  int age = 29;
+  print(age);
+
+  bool isOpen = true;
+  print(isOpen);
+
+  double rate = 7;
+  print(rate);
+
+  int? rollNo;
+  print(rollNo);
+}
