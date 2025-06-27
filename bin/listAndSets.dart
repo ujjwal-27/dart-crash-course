568beffe2454d49be46f19c147b2ee44cde12ac7
Ujjwal <ujjwalshresth.96@gmail.com>
2025-06-27 12:56:52 +0545

    sets

--- bin/listAndSets.dart
+++ bin/listAndSets.dart
@@ -13,4 +13,13 @@ void main() {
   for (int i = 0; i < scores.length; i++) {
     print(scores.elementAt(i));
   }
+
+  Set<String> animals = {"cat", "lion", "dog", "cow", "lion"};
+
+  animals.add("tiger");
+  animals.add("cow");
+
+  print(animals.add("mouse"));
+
+  print(animals);
 }
