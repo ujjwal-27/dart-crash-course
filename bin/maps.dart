a9fab59e86cf14f72a4f292e88ef9192f628653f
Ujjwal <ujjwalshresth.96@gmail.com>
2025-06-27 18:47:51 +0545

    maps

---
+++ bin/maps.dart
@@ -0,0 +1,13 @@
+void main() {
+  Map<String, String> planets = {
+    "first": "mercury",
+    "second": "venus",
+    "third": "earth",
+    "fourth": "mars",
+    "fifth": "jupiter",
+  };
+
+  print(planets["third"]);
+
+  print(planets);
+}
